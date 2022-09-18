8c2b82553169216f1d86ae8173ec9f61c52e3431
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-09-18 16:59:08 +0000

    structured page layout

---
+++ src/components/SideContents/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SideContents = () => {
+  return <div>SideContents</div>;
+};
+
+export default SideContents;
--- src/components/SideNav/index.jsx
+++ src/components/SideNav/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./SideNav.css";
 const SideNav = () => {
   return <div>SideNav</div>;
 };
