2d6f08d78a2ebbf81f40476170449ab17fd9b4f1
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-09-18 16:59:08 +0000

    structured page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import Navbar from "./components/Navbar";
+import SideNav from "./components/SideNav";
+import Footer from "./components/Footer";
+import SideContents from "./components/SideContents";
+import { Box, Grid } from "@mui/material";
 const App = () => {
   return (
     <main>
+      <Navbar />
       <div className="wrapper">
-        <Navbar />
+        <Grid container direction="row" spacing={2} my={2}>
+          <Grid item xs={4}>
+            <Box sx={{ backgroundColor: "yellow" }}>
+              <SideNav />
+            </Box>
+          </Grid>
+          <Grid item xs={8}>
+            <Box sx={{ backgroundColor: "pink" }}>
+              <SideContents />
+            </Box>
+          </Grid>
+        </Grid>
       </div>
+      <Footer />
     </main>
   );
 };
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./Slider.css";
 const Slider = () => {
   return <div> Slider</div>;
 };
