477388d17743b1e08a95a91419749eeea5d54dd6
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-09-21 13:59:01 +0100

    created initial setup for the slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Navbar from "./components/Navbar";
-import SideNav from "./components/SideNav";
-<<<<<<< HEAD
-=======
-
->>>>>>> 8c2b82553169216f1d86ae8173ec9f61c52e3431
-import Footer from "./components/Footer";
-import SideContents from "./components/SideContents";
-import { Box, Grid } from "@mui/material";
+import Navbar from './components/Navbar'
+import SideNav from './components/SideNav'
+import Footer from './components/Footer'
+import SideContents from './components/SideContents'
+import Slider from './components/Slider'
+import { Box, Grid } from '@mui/material'
+import phone_1 from './Assets/Images/phone_1.jpg'
+import phone_2 from './Assets/Images/phone_2.jpg'
+import phone_3 from './Assets/Images/phone_3.jpg'
+const cards = [
+  { url: phone_1, name: 'phone A' },
+  { url: phone_2, name: 'phone B' },
+  { url: phone_3, name: 'phone C' },
+  { url: phone_1, name: 'phone D' },
+  { url: phone_2, name: 'phone E' },
+  { url: phone_3, name: 'phone F' },
+  { url: phone_1, name: 'phone G' },
+  { url: phone_2, name: 'phone H' },
+  { url: phone_3, name: 'phone I' },
+  { url: phone_1, name: 'phone J' },
+]
 const App = () => {
   return (
     <main>
@@ -14,20 +26,21 @@ const App = () => {
       <div className="wrapper">
         <Grid container direction="row" spacing={2} my={2}>
           <Grid item xs={4}>
-            <Box sx={{ backgroundColor: "yellow" }}>
+            <Box sx={{ backgroundColor: 'yellow' }}>
               <SideNav />
             </Box>
           </Grid>
           <Grid item xs={8}>
-            <Box sx={{ backgroundColor: "pink" }}>
+            <Box sx={{ backgroundColor: 'pink' }}>
               <SideContents />
             </Box>
           </Grid>
         </Grid>
       </div>
+      <Slider cards={cards} />
       <Footer />
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-import "./Slider.css";
-const Slider = () => {
-  return <div> Slider</div>;
-};
+import React from 'react'
+import './Slider.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 
-export default Slider;
+// Swiper CSS
+import 'swiper/swiper.min.css'
+import 'swiper/modules/navigation/navigation.min.css'
+import 'swiper/modules/pagination/pagination.min.css'
+import 'swiper/modules/scrollbar/scrollbar.min.css'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
+
+const Slider = () => {
+  return (
+    <div className="container">
+      <Swiper
+        freeMode={true}
+        grabCursor={true}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        className="mySwiper"
+        slidesPerView={4}
+        spaceBetween={30}
+      >
+        {/* <SwiperSlide></SwiperSlide> */}
+      </Swiper>
+    </div>
+  )
+}
+
+export default Slider
