8a4805e01a2bfadf2e15bbd032345c06f308c4ed
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-09-28 12:54:13 +0100

    removed unused dependencies in productCard and Slider

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -5,6 +5,7 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import { Button, CardActionArea, CardActions } from '@mui/material/'
+
 function ProductCard({ image, name, price }) {
   return (
     <Card sx={{ maxWidth: 200 }} className="slider_card">
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Slider.css'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import SwiperCore, { Navigation, A11y } from 'swiper'
 import ProductCard from '../ProductCard'
 import phone_1 from '../../Assets/Images/phone_1.jpg'
 import phone_2 from '../../Assets/Images/phone_2.jpg'
@@ -10,23 +10,22 @@ import phone_3 from '../../Assets/Images/phone_3.jpg'
 // Swiper CSS
 import 'swiper/swiper.min.css'
 import 'swiper/modules/navigation/navigation.min.css'
-import 'swiper/modules/pagination/pagination.min.css'
-import 'swiper/modules/scrollbar/scrollbar.min.css'
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
+
+SwiperCore.use([Navigation, A11y])
 
 const cards = {
   category: 'Bestsellers',
   data: [
-    { image: phone_1, name: 'phone A', price: '199.99' },
-    { image: phone_2, name: 'phone B', price: '439.99' },
-    { image: phone_3, name: 'phone C', price: '549.99' },
-    { image: phone_1, name: 'phone D', price: '499.99' },
-    { image: phone_2, name: 'phone E', price: '79.99' },
-    { image: phone_3, name: 'phone F', price: '649.99' },
-    { image: phone_1, name: 'phone G', price: '999.99' },
-    { image: phone_2, name: 'phone H', price: '1499.99' },
-    { image: phone_3, name: 'phone I', price: '349.99' },
-    { image: phone_1, name: 'phone J', price: '999.99' },
+    { image: phone_1, name: 'Product A', price: '199.99' },
+    { image: phone_2, name: 'Product B', price: '439.99' },
+    { image: phone_3, name: 'Product C', price: '549.99' },
+    { image: phone_1, name: 'Product D', price: '499.99' },
+    { image: phone_2, name: 'Product E', price: '79.99' },
+    { image: phone_3, name: 'Product F', price: '649.99' },
+    { image: phone_1, name: 'Product G', price: '999.99' },
+    { image: phone_2, name: 'Product H', price: '1499.99' },
+    { image: phone_3, name: 'Product I', price: '349.99' },
+    { image: phone_1, name: 'Product J', price: '999.99' },
   ],
 }
 
@@ -38,7 +37,7 @@ const Slider = () => {
         <Swiper
           navigation={true}
           grabCursor={true}
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          modules={[Navigation, A11y]}
           className="mySwiper"
           breakpoints={{
             0: {
