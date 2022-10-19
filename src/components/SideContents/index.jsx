7ad5253b18fe6f2e46eecb7730a90664dce68a00
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2022-10-19 21:26:52 +0100

    Auto Slider added

--- src/components/SideContents/index.jsx
+++ src/components/SideContents/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Slider from "../Slider";
 import Collections from "../Collections";
+import BigSlider from "../BigSlider/BigSlider";
 const SideContents = () => {
   return (
     <>
+      <BigSlider />
       <Slider />
       <Collections />
     </>
